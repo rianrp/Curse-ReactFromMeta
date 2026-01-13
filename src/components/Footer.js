5a8d61d4ea762570371bba2cfb378e7ef0e76dc9
rianrp <67884987+rianrp@users.noreply.github.com>
2026-01-13 15:12:38 -0300

    Exercício: Configurando o documento HTML semântico

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <img src="logo.png" alt="Little Lemon Logo" />
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <p>Address: 123 Lemon Street</p>
+        <p>Phone: (555) 123-4567</p>
+        <p>Email: info@littlelemon.com</p>
+      </div>
+      <div>
+        <h3>Social Media</h3>
+        <ul>
+          <li><a href="/">Facebook</a></li>
+          <li><a href="/">Instagram</a></li>
+          <li><a href="/">Twitter</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to Little Lemon</h1>
+      <p>Reserve your table now!</p>
+    </main>
+  );
+}
+
+export default Main;
